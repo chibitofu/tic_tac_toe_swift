0d9c7db7604aac50c9c0de81fde978a52c6a583c
chibitofu <erinmoony@gmail.com>
2017-02-24 23:17:55 -0800

    First tile connected.

--- Tic_Tac_Toe_ios/ViewController.swift
+++ Tic_Tac_Toe_ios/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var playerMove: String = "x"
+    
+    @IBAction func tile(_ sender: UIButton) {
+        if playerMove == "x" {
+            print("\(playerMove)")
+            playerMove = "o"
+        } else {
+            print("\(playerMove)")
+            playerMove = "x"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
